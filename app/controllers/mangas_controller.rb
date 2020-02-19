f588ace87e0c310418bc068a1565658981797435
hibiki20600 <hibiki_inobe@icloud.com>
2020-02-19 14:29:11 +0900

    Update mangas_controller.rb

--- app/controllers/mangas_controller.rb
+++ app/controllers/mangas_controller.rb
@@ -10,11 +10,11 @@ class MangasController < ApplicationController
   end
 
   def create
-    @tags_box = Manga.t_split( manga_params[:tags_attributes][:"0"][:tag])
-    Manga.create(manga_params)
+    @tags_box = Manga.t_split( params[:manga][:tags_attributes][:"0"][:tag])
+    manga = Manga.create(manga_params)
     @tags_box.each do |tag|
       if tag
-        Tag.create(manga_id: params[:id], tag: tag)
+        Tag.create(manga_id: manga.id, tag: tag)
       end
     end
     redirect_to root_path
@@ -22,7 +22,7 @@ class MangasController < ApplicationController
   
   private
   def manga_params
-    params.require(:manga).permit(:title, :comment, :image, :author, tags_attributes: [:tag])
+    params.require(:manga).permit(:title, :comment, :image, :author)
   end
 
 end
